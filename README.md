1b5b1f59c5ad1084ec2403eeedbd01ce08f75641
DymuX17 <patrykd1707@gmail.com>
2023-12-09 00:09:52 +0100

    testowanie

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # imu-simulation
+
+Imu simulator in Python and data reader is c++
---
+++ imu-data/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int a, b;
+    std::cout << "Podaj wartosc a: ";
+    std::cin >> a;
+    std::cout << "wartosc a wynosi: " << a << std::endl;
+    return 0;
+}
